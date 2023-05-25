21d70d6859a618f9adcfa84038cd1eb5068dc86d
Andrés Olivé <andresolive@gmail.com>
2023-05-25 16:06:31 -0600

    commented orders schema

--- schema/orders.js
+++ schema/orders.js
@@ -1,56 +1,56 @@
-cube(`orders`, {
-  sql_table: `public.orders`,
-  
-  data_source: `default`,
-  
-  pre_aggregations: {
-    // Pre-aggregation definitions go here
-    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
-  },
-  
-  joins: {
-    users: {
-      sql: `${CUBE}.user_id = ${users}.id`,
-      relationship: `many_to_one`
-    },
-    
-    products: {
-      sql: `${CUBE}.product_id = ${products}.id`,
-      relationship: `many_to_one`
-    }
-  },
-  
-  measures: {
-    count: {
-      type: `count`
-    },
-    
-    number: {
-      sql: `number`,
-      type: `sum`
-    }
-  },
-  
-  dimensions: {
-    id: {
-      sql: `id`,
-      type: `number`,
-      primary_key: true
-    },
-    
-    status: {
-      sql: `status`,
-      type: `string`
-    },
-    
-    created_at: {
-      sql: `created_at`,
-      type: `time`
-    },
-    
-    completed_at: {
-      sql: `completed_at`,
-      type: `time`
-    }
-  }
-});
+// cube(`orders`, {
+//   sql_table: `public.orders`,
+
+//   data_source: `default`,
+
+//   pre_aggregations: {
+//     // Pre-aggregation definitions go here
+//     // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
+//   },
+
+//   joins: {
+//     users: {
+//       sql: `${CUBE}.user_id = ${users}.id`,
+//       relationship: `many_to_one`
+//     },
+
+//     products: {
+//       sql: `${CUBE}.product_id = ${products}.id`,
+//       relationship: `many_to_one`
+//     }
+//   },
+
+//   measures: {
+//     count: {
+//       type: `count`
+//     },
+
+//     number: {
+//       sql: `number`,
+//       type: `sum`
+//     }
+//   },
+
+//   dimensions: {
+//     id: {
+//       sql: `id`,
+//       type: `number`,
+//       primary_key: true
+//     },
+
+//     status: {
+//       sql: `status`,
+//       type: `string`
+//     },
+
+//     created_at: {
+//       sql: `created_at`,
+//       type: `time`
+//     },
+
+//     completed_at: {
+//       sql: `completed_at`,
+//       type: `time`
+//     }
+//   }
+// });
